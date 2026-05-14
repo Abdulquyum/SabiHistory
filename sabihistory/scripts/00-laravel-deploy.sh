ed0af9a878814c9694504a29e9859fb2a7b12f26
Abdulquyum <ajumobiabdulquyum@gmail.com>
2026-05-14 11:09:35 +0100

    deploy to render

---
+++ sabihistory/scripts/00-laravel-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Running composer install (without dev dependencies)"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching Laravel configuration"
+php artisan config:cache
+
+echo "Caching Laravel routes"
+php artisan route:cache
+
+echo "Running database migrations"
+php artisan migrate --force
+
+echo "Laravel deployment script finished"
